b340e9b0b4cf1c034c8dbf5e183ad57bd4b9964a
sjonesarts <85701152+sjonesarts@users.noreply.github.com>
2021-08-19 20:48:01 -0400

    semicolons!!!!

--- studio/Rocket.js
+++ studio/Rocket.js
@@ -8,20 +8,16 @@ var Rocket = /** @class */ (function () {
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
     }
-    ;
     Rocket.prototype.sumMass = function (items) {
         var sum = 0;
         for (var i = 0; i < items.length; i++) {
             sum += items[i].massKg;
         }
-        ;
         return sum;
     };
-    ;
     Rocket.prototype.currentMassKg = function () {
         return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
     };
-    ;
     Rocket.prototype.canAdd = function (item) {
         if (this.currentMassKg() + item.massKg <= this.totalCapacityKg) {
             return true;
@@ -29,20 +25,16 @@ var Rocket = /** @class */ (function () {
         else {
             return false;
         }
-        ;
     };
-    ;
     Rocket.prototype.addCargo = function (cargo) {
         if (this.canAdd(cargo) === true) {
-            return this.cargoItems.push(cargo);
+            this.cargoItems.push(cargo);
+            return true;
         }
         else {
-            console.log("cargo fail");
             return false;
         }
-        ;
     };
-    ;
     Rocket.prototype.addAstronaut = function (astronaut) {
         if (this.canAdd(astronaut) === true) {
             return this.astronauts.push(astronaut);
@@ -50,9 +42,7 @@ var Rocket = /** @class */ (function () {
         else {
             return false;
         }
-        ;
     };
-    ;
     return Rocket;
 }());
 exports.Rocket = Rocket;
